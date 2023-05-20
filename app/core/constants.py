4a19b830ddebf09aa82b7ca937b8c16a091a88f8
Goda <goda.choese@gmail.com>
2023-05-20 15:39:52 +0900

    refactor(core): Remove typevar R
    Signed-off-by: Goda <goda.choese@gmail.com>

--- app/core/constants.py
+++ app/core/constants.py
@@ -3,7 +3,5 @@ from typing import Awaitable, Callable, TypeVar
 from typing_extensions import ParamSpec
 
 P = ParamSpec("P")
-R = TypeVar("R")
-CORO_FUNC = Callable[P, Awaitable[R]]
-
 T = TypeVar("T")
+CORO_FUNC = Callable[P, Awaitable[T]]
