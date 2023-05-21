6ba1c1b5a3c554dc757fe616ce573b6918619294
Goda <goda.choese@gmail.com>
2023-05-21 15:37:28 +0900

    test(database): Reduce iteration count from test_aioperf
    Signed-off-by: Goda <goda.choese@gmail.com>

--- tests/test_database/__init__.py
+++ tests/test_database/__init__.py
@@ -1,3 +1,4 @@
 from .dependencies import TempCommander, TempQuerier
 from .models import Temp
+from .routers import router
 from .services import TempAggregateService, TempCommandService, TempQueryService
