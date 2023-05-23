024b0f16508a8ce66dc187b5544bdb9a8de8a880
Goda <goda.choese@gmail.com>
2023-05-24 07:28:54 +0900

    feat(gpt): Move default value to client to schema
    Signed-off-by: Goda <goda.choese@gmail.com>

--- app/gpt/clients.py
+++ app/gpt/clients.py
@@ -2,33 +2,17 @@ import openai
 
 from app.core.clients import Client
 
-from .schemas import (
-    ChatCompletionRequest,
-    ChatCompletionResponse,
-    ChatModel,
-    Message,
-    Role,
-)
+from .schemas import ChatCompletionRequest, ChatCompletionResponse, Message, Role
 
 
 class GptClient(Client):
-    model: ChatModel = ChatModel.gpt3dot5
-    max_tokens: int = 512
-    temperature: float = 1.0
-    top_p: float = 1.0
-    n: int = 1
-
-    async def chat(self, *, prompt: str) -> str:
+    @staticmethod
+    async def chat(*, prompt: str) -> str:
         completion_request = ChatCompletionRequest(
-            model=self.model,
             messages=[
                 Message(role=Role.system, content="You are a helpful chatbot named godabot."),
                 Message(role=Role.user, content=prompt),
-            ],
-            max_tokens=self.max_tokens,
-            temperature=self.temperature,
-            top_p=self.top_p,
-            n=self.n,
+            ]
         )
         response = await openai.ChatCompletion.acreate(**completion_request.dict())
         return ChatCompletionResponse.from_orm(response).answer()
--- app/gpt/schemas.py
+++ app/gpt/schemas.py
@@ -23,12 +23,12 @@ class Message(Schema):
 
 
 class ChatCompletionRequest(Schema):
-    model: ChatModel
+    model: ChatModel = ChatModel.gpt3dot5
     messages: list[Message]
-    max_tokens: int
-    temperature: float
-    top_p: float
-    n: int
+    max_tokens: int = 512
+    temperature: float = 1.0
+    top_p: float = 1.0
+    n: int = 1
 
 
 class ChatCompletionResponse(Schema):
