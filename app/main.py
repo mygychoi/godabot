7296101a88c68484a9129455f68990ecc2e54116
Goda <goda.choese@gmail.com>
2023-05-29 19:13:19 +0900

    test(main): test env
    Signed-off-by: Goda <goda.choese@gmail.com>

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ import logging
 from fastapi import FastAPI
 from fastapi.exception_handlers import request_validation_exception_handler
 from fastapi.exceptions import RequestValidationError
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 from app import access, home, slashcommand
 from app.configs import settings
 from app.core.database.pool import PoolManager
 from app.core.middleware import TrustedRequestMiddleware
+
+# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+
 
 godabot = FastAPI(
     title=settings.NAME,
@@ -23,7 +25,7 @@ godabot = FastAPI(
 )
 
 # Middlewares
-godabot.add_middleware(HTTPSRedirectMiddleware)
+# godabot.add_middleware(HTTPSRedirectMiddleware)
 godabot.add_middleware(TrustedRequestMiddleware)
 
 
