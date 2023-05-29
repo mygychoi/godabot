37bbd9c784e6b907c8cd9c2b802a34fa344d1beb
Goda <goda.choese@gmail.com>
2023-05-29 19:07:02 +0900

    Revert "refactor(main): Turn off HTTPSRedirectMiddleware"
    This reverts commit 8a035ce858cc127b32ab6156af3ec8e5da36e6db.

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import logging
 from fastapi import FastAPI
 from fastapi.exception_handlers import request_validation_exception_handler
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 from app import access, home, slashcommand
 from app.configs import settings
@@ -23,6 +24,7 @@ godabot = FastAPI(
 
 # Middlewares
 if settings.ENV == settings.PROD:
+    godabot.add_middleware(HTTPSRedirectMiddleware)
     godabot.add_middleware(TrustedRequestMiddleware)
 
 
