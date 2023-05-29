f669b4a2ebae40cc0d0f0208168e2702d9f4f7ef
Goda <goda.choese@gmail.com>
2023-05-29 18:21:40 +0900

    fix(access): Fix typo
    Signed-off-by: Goda <goda.choese@gmail.com>

--- app/access/clients.py
+++ app/access/clients.py
@@ -10,7 +10,7 @@ class AccessClient(Client):
     client: AsyncWebClient = AsyncWebClient()
 
     async def acquire_access(self, *, code: str) -> AccessFormResult:
-        response = await self.client.oauth_access(
+        response = await self.client.oauth_v2_access(
             client_id=settings.SLACK_CLIENT_ID,
             client_secret=settings.SLACK_CLIENT_SECRET,
             code=code,
