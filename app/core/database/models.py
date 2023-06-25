7e3c127643fd0a7e2465273f7d415a3812df0f7b
Goda <goda.choese@gmail.com>
2023-06-25 22:13:29 +0900

    feat(database): Add update method to Model
    Signed-off-by: Goda <goda.choese@gmail.com>

--- app/core/database/models.py
+++ app/core/database/models.py
@@ -1,4 +1,6 @@
-from typing import Self
+from __future__ import annotations
+
+from typing import Self, TypeVar
 
 from asyncpg import Record
 from pydantic import BaseModel
@@ -26,3 +28,15 @@ class Model(BaseModel, Registry):
             return None
         else:
             return cls.parse_obj(obj)
+
+    @classmethod
+    def update_many_from(cls, *, models: list[ModelType], records: list[Record]):
+        for i in range(len(records)):
+            models[i].update_from(record=records[i])
+
+    def update_from(self, *, record: Record):
+        for field, value in record.items():
+            setattr(self, field, value)
+
+
+ModelType = TypeVar("ModelType", bound=Model)
